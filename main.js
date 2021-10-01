aa1a5482b115a21a0f1f14547d95e55cb6ec73f6
Opliko <opliko.reg@protonmail.com>
2021-10-01 17:28:10 +0000

    feat: last features and tweaks

--- main.js
+++ main.js
@@ -2,15 +2,16 @@
 const price = 8;
 const height = 2.7;
 function runScript() {
-    const sendButton = document.getElementById("send");
+    const form = document.getElementById("contact-form");
     const nameInput = document.querySelector('input[name="name"]');
     const surnameInput = document.querySelector('input[name="surname"]');
     const emailInput = document.querySelector('input[name="email"]');
     const serviceSelect = document.querySelector('select[name="service"]');
     const result = document.getElementById("result");
-    sendButton.addEventListener("click", () => {
-        const name = nameInput.value.toLowerCase();
-        const surname = surnameInput.value.toLowerCase();
+    form.addEventListener("submit", (e) => {
+        e.preventDefault();
+        const name = nameInput.value;
+        const surname = surnameInput.value;
         const email = emailInput.value.toLowerCase();
         const service = serviceSelect.value;
         result.innerHTML = `${name} ${surname}<br>${email}<br>Usługa: ${service}`;
